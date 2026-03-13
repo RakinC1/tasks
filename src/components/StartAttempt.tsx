553c0e0e32aaad01306fd07469e2ce340edffe25
RakinC1 <uappstech@hotmail.com>
2026-03-12 20:22:54 -0400

    solved

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,29 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): React.JSX.Element {
-    return <div>Start Attempt</div>;
+   const [attempts,setAttempts] = useState<number>(4);
+   const [inProgess, setInProgress] = useState<boolean>(false);
+ 
+   function startQuiz(){
+    setAttempts(attempts-1);
+    setInProgress(true);
+   }
+
+   function stopQuiz(){
+    setInProgress(false);
+   }
+   function mulligan(){
+    setAttempts(attempts+1);
+   }
+
+   return(
+    <div>
+        <p>Attempts: {attempts}</p>
+
+        <Button onClick={startQuiz} disabled= {inProgess|| attempts===0}> Start quiz</Button>
+        <Button onClick={stopQuiz} disabled= {!inProgess}> Stop quiz</Button>
+        <Button onClick={mulligan} disabled={inProgess}>Mulligan</Button>
+    </div>
+   )
+
 }
--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -12,5 +12,36 @@ export function d6(): number {
 }
 
 export function TwoDice(): React.JSX.Element {
-    return <div>Two Dice</div>;
+    const [leftDie, setLeftDie] = useState<number>(1);
+    const [rightDie, setRightDie] = useState<number>(2);
+
+    function rollLeft(){
+        setLeftDie(d6());
+    }
+
+    function rollRight(){
+        setRightDie(d6())
+    }
+
+    let message = "";
+
+    if(leftDie === rightDie){
+        if (leftDie===1){
+            message = "Lose";
+        } else{
+            message= "Win";
+        }
+    }
+
+    return (
+        <div>
+            <span data-testid = "left-die">{leftDie}</span>
+            <span data-testid = "right-die">{rightDie}</span>
+            <div>
+                <Button onClick={rollLeft}>Roll left</Button>
+                <Button onClick={rollRight}>Roll right</Button>
+            </div>
+            <p>{message}</p>
+        </div>
+    );
 }
